e33b7153f08186072e65bf810ca701fd44f906d3
jakob.roessler@stud.uni-regensburg.de <jakobs3mini@gmail.com>
2025-12-12 16:20:35 +0100

    .

---
+++ ConcertApp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace ConcertApp;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        return new Window(new AppShell());
+    }
+}
